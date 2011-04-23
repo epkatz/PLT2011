316cd6cdd0344c2eba4d11abeb05b694c7843a95
elikatz <friendlyneighborhoodkatz@gmail.com>
2011-04-23 03:07:58 -0400

    Added most of the function, boolean and variable checking

--- Semantic_frontend/Function.java
+++ Semantic_frontend/Function.java
@@ -1,17 +1,20 @@
 import java.util.HashMap;
+import java.lang.Integer;
 
 
 public class Function {
 
-	private String functionName;
-	private HashMap<String, String> argsList = new HashMap<String, String>(); //name type
-	private String returnType;
-	private HashMap<String, String> varList = new HashMap<String, String>(); //name, type
+	String functionName;
+	HashMap<String, String> argsList = new HashMap<String, String>(); //name type
+	String returnType;
+	static boolean debugging = true;
 	
-	//Parse and set
+	/* Constructor sets a function's name, returnType and parameters in the instance variables of the function */
 	public Function(String functionName, String returnType, String paramList){
 		this.functionName = functionName;
+		if (debugging){System.out.println("**Initializing " + functionName + " function**");}
 		this.returnType = returnType;
+		if (debugging){System.out.println(functionName + "= returnType: " + returnType);}
 		String[] params = paramList.split(",");
 		String[] temp;
 		for(int i=0; i<params.length; i++){
@@ -20,42 +23,14 @@ public class Function {
 				params[i] = params[i].substring(1);
 			//split
 			temp = params[i].split(" ");
-			argsList.put(temp[0], temp[1]);
+			argsList.put(temp[1], temp[0]); //This is reversed in the argument list so reversing it back here
+			if (debugging) {System.out.println(functionName + "= argName: " + temp[1] + ", type: " + temp[0]);}
 		}
 	}
 	
-	//Check Divide by zero
-	public boolean checkDivideByZero(String var1, String var2){
-		for(;;)
-			return true;
-	}
-	
-	//Check whether the return type is the same as the returnProduction
+	/* Check whether the return type is the same as the returnProduction */
 	public boolean checkReturnType(String returnProduction){
 		return returnType.equals(returnProduction);
-	}
-	
-	//Check whether a variable used exists and is the same as its declared type
-	public boolean varExists(String varName, String varType){
-		if(varList.containsKey(varName)){
-			return varType.equals(varList.get(varName));
-		}
-		else
-			return false;
-	}
-	
-	//Setters and Getters for VarList
-	public boolean addVar(String varName, String varType){
-		varList.put(varName, varType);
-		return true;
-	}
-	public boolean setVarType(String varName, String varType){
-		varList.remove(varName);
-		varList.put(varName, varType);
-		return true;
-	}
-	public String getVar(String varName){
-		return varList.get(varName);
 	}
 	
 	public String getFunctionName() {
@@ -63,12 +38,6 @@ public class Function {
 	}
 	public void setFunctionName(String functionName) {
 		this.functionName = functionName;
-	}
-	public HashMap<String, String> getArgsList() {
-		return argsList;
-	}
-	public void setArgsList(HashMap<String, String> argsList) {
-		this.argsList = argsList;
 	}
 	public String getReturnType() {
 		return returnType;
