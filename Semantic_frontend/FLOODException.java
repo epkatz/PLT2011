78f81ea5b96d2dc1b1b81b437e2dc665281c81ab
Stephanie Aligbe <sna2111.cs@gmail.com>
2011-05-04 15:32:55 -0400

    function semantics fist draft

--- Semantic_frontend/FLOODException.java
+++ Semantic_frontend/FLOODException.java
@@ -13,6 +13,11 @@ public class FLOODException extends Exception {
 		FLOODException e = new FLOODException(lineInfo, "At line: " + lineInfo + ", " + element + " variable Already Exists");
 		return e;		
 	}
+	
+	public static FLOODException InvalidFunctionReturnType(int lineInfo, String element) throws FLOODException{
+		FLOODException e = new FLOODException(lineInfo, "At line: " + lineInfo + ", " + element + " invalid return ");
+		return e;
+	}
 
 	public String getError(){
 		return message;
--- Semantic_frontend/Function.java
+++ Semantic_frontend/Function.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
+import java.util.StringTokenizer;
 import java.lang.Integer;
+import java.util.ArrayList;
 
 
 public class Function {
@@ -7,14 +9,16 @@ public class Function {
 	String functionName;
 	HashMap<String, String> argsList = new HashMap<String, String>(); //name type
 	String returnType;
+	int lineNumber;
 	static boolean debugging = true;
 	
 	/* Constructor sets a function's name, returnType and parameters in the instance variables of the function */
-	public Function(String functionName, String returnType, String paramList){
+	public Function(String functionName, String returnType, String paramList, int lineNumber){
 		this.functionName = functionName;
 		if (debugging){System.out.println("**Initializing " + functionName + " function**");}
 		this.returnType = returnType;
 		if (debugging){System.out.println(functionName + "= returnType: " + returnType);}
+		this.lineNumber = lineNumber;
 		String[] params = paramList.split(",");
 		String[] temp;
 		for(int i=0; i<params.length; i++){
@@ -31,6 +35,72 @@ public class Function {
 	/* Check whether the return type is the same as the returnProduction */
 	public boolean checkReturnType(String returnProduction){
 		return returnType.equals(returnProduction);
+	}
+	
+	/* Check whether the return type is the same as the actual return */
+	public boolean checkActualReturn(String returnProduction){
+		//get return value
+		String returnVal = returnProduction.replaceAll("return ", "");
+		returnVal = returnVal.replaceAll(";", "");
+		
+		//find in argument list
+		if(argsList.containsKey(returnVal)){
+			return returnType.equals(argsList.get(returnVal));
+		}
+		//if not in argument list the check constant type
+		else{
+			//if starts and ends with "
+			if(returnVal.startsWith("\"") && returnVal.endsWith("\""))
+				return  returnType.equals("str");
+			//if starts and ends with '
+			if(returnVal.startsWith("\'") && returnVal.endsWith("\'"))
+				return  returnType.equals("str");
+			//check if float
+			if(!returnVal.contains(".")){
+				try{
+					Integer.parseInt(returnVal);
+					return returnType.equals("Int");
+				}
+				catch(Exception e){
+					return false;
+				}
+			}
+			else{
+				try{
+					Float.parseFloat(returnVal);
+					return returnType.equals("flt");
+				}
+				catch(Exception e){
+					return false;
+				}
+			}
+		}
+	}
+	
+	//check that function returns
+	public boolean functionReturns(String returnProduction){
+		//get return value
+		String returnVal = returnProduction.replaceAll("return ", "");
+		returnVal = returnVal.replaceAll(";", "");
+		return (returnVal.length()>0);
+	}
+	
+	//write required functions
+	
+	public void writeDraftFunction(){
+		
+	}
+	
+	public void writeDraftPlayer(){
+		
+	}
+	
+	public void writeTrade(){
+		
+	}
+	
+	public void writeDropPlayer(){
+		
 	}
 	
 	public String getFunctionName() {
