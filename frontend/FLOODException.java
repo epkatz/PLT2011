749c799b7c3590164e3026e0ae4927a60491c227
elikatz <friendlyneighborhoodkatz@gmail.com>
2011-05-04 18:00:37 -0400

    changed semantic locations:w

---
+++ frontend/FLOODException.java
@@ -0,0 +1,25 @@
+import java.util.Hashtable;
+
+public class FLOODException extends Exception {
+	private String message = null;
+	private String lineInfo = null;
+	
+	public FLOODException(int lineInfo, String message){
+		this.lineInfo = lineInfo + "";
+		this.message = message;
+	}
+
+	public static FLOODException VariableAlreadyExists(int lineInfo, String element) throws FLOODException{
+		FLOODException e = new FLOODException(lineInfo, "At line: " + lineInfo + ", " + element + " variable Already Exists");
+		return e;		
+	}
+	
+	public static FLOODException InvalidFunctionReturnType(int lineInfo, String element) throws FLOODException{
+		FLOODException e = new FLOODException(lineInfo, "At line: " + lineInfo + ", " + element + " invalid return ");
+		return e;
+	}
+
+	public String getError(){
+		return message;
+	}
+}
